55ea8ebe1effeb606d3658114a5612be2b99330d
sejongPark <lifedesigner88@gmail.com>
2024-08-27 21:50:46 +0900

    test

---
+++ src/main/java/ControllerTest.java
@@ -0,0 +1,14 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class ControllerTest {
+
+    @GetMapping
+    public String test(){
+        return "OK";
+    }
+
+}
