9f9f1e9ad1ec1ffd9cb493cd547af18b2db5570b
SejongPark <lifedesigner88@gmail.com>
2024-08-29 16:49:41 +0900

    BackEnd CORS Plus

--- src/main/java/io/cloudtype/Demo/Configs/WebConfig.java
+++ src/main/java/io/cloudtype/Demo/Configs/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173","https://web-vuetest-m0ce3xjwfbac249f.sel4.cloudtype.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
